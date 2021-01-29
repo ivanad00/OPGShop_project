73727d7259ddeff8c7731f0097d1cfeddac1d9e8
igaspa <62906300+igaspa@users.noreply.github.com>
2021-01-29 20:50:53 +0100

    Add files via upload

---
+++ Models/OrderProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OPG.Models;
+
+
+namespace OPG.Models
+{
+    public class OrderProduct
+    {
+        public int OrderProductId { get; set; }
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OPG.Models;
+
+namespace OPG.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress1 { get; set; }
+        public string Adress2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int ZipCode { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
