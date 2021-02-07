aca2ba0ea316e4e4d9cd9fdcd73efc05febc593b
ivana <ivana.djipalo.00@fesb.hr>
2021-02-07 21:17:56 +0100

    frontend fixed

---
+++ Migrations/20210207170218_opgApp.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OPG.Migrations
+{
+    public partial class opgApp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "amount",
+                table: "OrderItems",
+                newName: "Amount");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "Name",
+                value: "Apple");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "OrderItems",
+                newName: "amount");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "Name",
+                value: "Apple ");
+        }
+    }
+}
