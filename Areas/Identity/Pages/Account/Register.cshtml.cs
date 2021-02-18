3c73f750145b9ec17d8ae69d3927d9ad8c33100c
ivana <ivana.djipalo.00@fesb.hr>
2021-02-18 16:24:48 +0100

    register

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,14 @@ namespace OPG.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
