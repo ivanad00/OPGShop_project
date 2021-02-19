b54f1fc28a5cc59ab77eb4807f3cc294427b5784
ivana <ivana.djipalo.00@fesb.hr>
2021-02-19 13:12:05 +0100

    feedback

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,20 +1,20 @@
 
-/* register / login button */
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
+///* register / login button */
+//var x = document.getElementById("login");
+//var y = document.getElementById("register");
+//var z = document.getElementById("btn");
 
-function register() {
-    x.style.left = "-400px";
-    y.style.left = "50px";
-    z.style.left = "110px";
-}
+//function register() {
+//    x.style.left = "-400px";
+//    y.style.left = "50px";
+//    z.style.left = "110px";
+//}
 
-function login() {
-    x.style.left = "50px";
-    y.style.left = "450px";
-    z.style.left = "0px";
-}
+//function login() {
+//    x.style.left = "50px";
+//    y.style.left = "450px";
+//    z.style.left = "0px";
+//}
 
 /* nav */
 
@@ -32,16 +32,18 @@ function redirectingForms() {
 
 
 
-function showPayment() {
-    var x = document.getElementById("checkout");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+//function showPayment() {
+//    var x = document.getElementById("checkout");
+//    if (x.style.display === "none") {
+//        x.style.display = "block";
+//    } else {
+//        x.style.display = "none";
+//    }
+//}
 
 
+
+/* back to top */
 var back2Top = document.getElementById("back2Top");
 back2Top.style.display = "none";  // hidden by default
 document.querySelector('body').onscroll = function () {
@@ -50,3 +52,59 @@ document.querySelector('body').onscroll = function () {
     else
         back2Top.style.display = "none";
 };
+
+
+const starElements = document.querySelectorAll(".star .heart-icon");
+for (let i = 0; i < starElements.length; i++) {
+    const starElement = starElements[i];
+    starElement.addEventListener("click", handleStarClick);
+}
+
+function handleStarClick(e) {
+    const star = e.currentTarget;
+    const starContainer = star.parentElement;
+    const starSiblings = starContainer.children;
+
+    let clickedStarPassed = false;
+    for (let i = 0; i < starSiblings.length; i++) {
+        const currentStar = starSiblings[i];
+
+        if (!clickedStarPassed) {
+            currentStar.classList.remove("far");
+            currentStar.classList.add("fas");
+        }
+        else {
+            currentStar.classList.remove("fas");
+            currentStar.classList.add("far");
+        }
+
+        if (currentStar == star) {
+            clickedStarPassed = true;
+        }
+    }
+}
+
+var count;
+
+function starmark(item) {
+    count = item.id[0];
+    sessionStorage.starRating = count;
+    var subid = item.id.substring(1);
+    for (var i = 0; i < 5; i++) {
+        if (i < count) {
+            document.getElementById((i + 1) + subid).style.color = "orange";
+        }
+        else {
+            document.getElementById((i + 1) + subid).style.color = "white";
+        }
+
+
+    }
+
+}
+
+//function result() {
+//    //Rating : Count
+//    //Review : Comment(id)
+//    alert("Rating : " + count + "\nReview : " + document.getElementById("comment").value);
+//}
